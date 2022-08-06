e5d26b3c42e913404c3f85ca1cea90df5c657339
Lucas Hlongwane <LucasHlongwane6@gmail.com>
2022-08-06 11:19:18 +0200

    bug fix

--- Controllers/CoinJarsController.cs
+++ Controllers/CoinJarsController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Net;  
 using System.Net.Http;  
 using System.Web.Http; 
+using Microsoft.AspNet.WebApi;
 
 namespace CoinJarApiAssessment.Controllers
 {
+    [ApiController]
+    [Route("api/CoinJar")]
     public class CoinJarsController: ApiController
     {
-
+        
      
 
     }
--- Models/CoinJar.cs
+++ Models/CoinJar.cs
@@ -6,7 +6,7 @@ public class CoinJar
     }
     private readonly Coin[] coins = Enum.GetValues(typeof(eCoinType)).Cast<eCoinType>().Select(x => new Coin(x)).ToArray(); 
 
-    public decimal GetTotalAmount() => coins.select(c => (decimal))
+    public decimal GetTotalAmount() => coins.select(c => (decimal)c.CoinType * (decimal)c.Amount).sum();
 
     public void Reset()
     {
